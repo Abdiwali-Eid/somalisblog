117f6155cc1638265d4f390da2b64c4d7b585906
Abdiwali-Eid <cabdiwalibashir232@gmail.com>
2023-06-29 15:01:45 +0300

    second commit

---
+++ web/src/components/homePage/TopCategories.js
@@ -0,0 +1,29 @@
+import { graphql, useStaticQuery } from 'gatsby';
+import React from 'react';
+import { TopCategoriesStyles } from '../../styles/homePage/TopCategoriesStyles';
+import CategoryGrid from '../category/CategoryGrid';
+import ParagraphText from '../typography/ParagraphText';
+import { SectionTitle } from '../typography/Title';
+
+function TopCategories() {
+  const data = useStaticQuery(graphql`
+    {
+      allSanityFeatured(filter: { _id: { eq: "featuredItems" } }) {
+        nodes {
+          category {
+            id
+            title
+            slug {
+              current
+            }
+            _rawDescription
+          }
+        }
+      }
+    }
+  `);
+  // const categories = data.allSanityFeatured.nodes[0].category;
+  return <div>top Catogry</div>;
+}
+
+export default TopCategories;
---
+++ web/src/styles/GlobalStyles.js
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  :root{
+    --primary: rgb(85, 128, 170);
+    --secondary: rgb(85, 128, 170);
+    --darkBlue: #070747; 
+    --darkPurple: #0E0034;
+    --black-1: rgb(240, 242, 245);
+    --black-2: #121826;
+    --white-1: #C6BED9;
+    --gray: #64718A;
+    --grey: var(--gray);
+    --white: #121826;
+    --black: black;
+  }
+  html{
+    font-size: 10px;
+  }
+  
+  body{
+    min-height: 100vh;
+    line-height: 1.5;
+    width: 100%;
+    font-family: 'Inter';
+    background: var(--black-1);
+    color: var(--white);
+  }
+  a{
+    text-decoration: none;
+    color: var(--white);
+  }
+  .container{
+    max-width: 1200px;
+    margin: 0 auto;
+    width: 90%;
+  }
+  img, svg{
+    height: 100%;
+    width: 100%;
+  }
+  li, ul{
+    list-style: none;
+  }
+`;
+
+export default GlobalStyles;
